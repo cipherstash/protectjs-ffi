a936955d3960cf15f18d466cc9d069dca5a4f4a8
Drew Thomas <drew@cipherstash.com>
2025-03-10 12:24:41 +1100

    Refactor: update `encrypt` to take an `EncryptPayload`
    This change refactors the `encrypt` function to combine the `plaintext`,
    `columnName`, `tableName`, and `context` args into a single
    `EncryptPayload` (similar to `encryptBulk`).
    
    This change also makes the `table` property required in the `EncryptPayload`
    and allows for passing in `serviceToken` as `undefined` to the Rust addon
    functions.

--- integration-tests/tests/index.test.ts
+++ integration-tests/tests/index.test.ts
@@ -3,7 +3,41 @@ import { expect, test } from 'vitest'
 import { decrypt, encrypt, newClient } from '@cipherstash/protect-ffi'
 
 test('can round-trip encrypt and decrypt', async () => {
-  const encryptConfig = {
+  const client = await newClient(encryptConfig())
+  const originalPlaintext = 'abc'
+
+  const ciphertext = await encrypt(client, {
+    plaintext: originalPlaintext,
+    column: 'email',
+    table: 'users',
+  })
+
+  const decrypted = await decrypt(client, JSON.parse(ciphertext).c)
+
+  expect(decrypted).toBe(originalPlaintext)
+})
+
+test('can pass in undefined for ctsToken', async () => {
+  const client = await newClient(encryptConfig())
+  const originalPlaintext = 'abc'
+
+  const ciphertext = await encrypt(
+    client,
+    {
+      plaintext: originalPlaintext,
+      column: 'email',
+      table: 'users',
+    },
+    undefined,
+  )
+
+  const decrypted = await decrypt(client, JSON.parse(ciphertext).c, undefined)
+
+  expect(decrypted).toBe(originalPlaintext)
+})
+
+function encryptConfig() {
+  return JSON.stringify({
     v: 1,
     tables: {
       users: {
@@ -16,13 +50,5 @@ test('can round-trip encrypt and decrypt', async () => {
         },
       },
     },
-  }
-
-  const client = await newClient(JSON.stringify(encryptConfig))
-
-  const ciphertext = await encrypt(client, 'abc', 'email', 'users')
-
-  const plaintext = await decrypt(client, JSON.parse(ciphertext).c)
-
-  expect(plaintext).toBe('abc')
-})
+  })
+}
