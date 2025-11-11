ee5c099a59a0058bf24ca73da64d3e0b56c67b9c
CJ Brewer <brewercalvinj@gmail.com>
2025-11-11 15:04:29 -0700

    chore: add mroe tests

--- integration-tests/tests/keyset.test.ts
+++ integration-tests/tests/keyset.test.ts
@@ -9,9 +9,7 @@ import {
   isEncrypted,
 } from '@cipherstash/protect-ffi'
 
-// Import a shared encryptConfig from common.js
 import { encryptConfig } from './common.js'
-
 type UserColumn = Identifier<typeof encryptConfig>
 
 const stringColumn: UserColumn = {
@@ -19,24 +17,63 @@ const stringColumn: UserColumn = {
   column: 'email',
 }
 
-test('can round-trip encrypt and decrypt a string using keyset', async () => {
-  const client = await newClient({
-    encryptConfig,
-    clientOpts: {
-      keyset: { Name: 'default' },
-    },
+describe('keyset tests', () => {
+  test('can round-trip encrypt and decrypt a string using keyset name', async () => {
+    const client = await newClient({
+      encryptConfig,
+      clientOpts: {
+        keyset: { Name: 'Test' },
+      },
+    })
+
+    const identifier = stringColumn
+    const plaintext = 'abc'
+
+    const ciphertext = await encrypt(client, {
+      plaintext,
+      ...identifier,
+    })
+
+    expect(isEncrypted(ciphertext)).toBe(true)
+
+    const decrypted = await decrypt(client, { ciphertext })
+    expect(decrypted).toBe(plaintext)
   })
 
-  const identifier = stringColumn
-  const plaintext = 'abc'
+  test('can round-trip encrypt and decrypt a string using keyset uuid', async () => {
+    const client = await newClient({
+      encryptConfig,
+      clientOpts: {
+        keyset: { Uuid: '4152449b-505a-4186-93b6-d3d87eba7a47' },
+      },
+    })
 
-  const ciphertext = await encrypt(client, {
-    plaintext,
-    ...identifier,
+    const identifier = stringColumn
+    const plaintext = 'abc'
+
+    const ciphertext = await encrypt(client, {
+      plaintext,
+      ...identifier,
+    })
+
+    expect(isEncrypted(ciphertext)).toBe(true)
+
+    const decrypted = await decrypt(client, { ciphertext })
+    expect(decrypted).toBe(plaintext)
   })
 
-  expect(isEncrypted(ciphertext)).toBe(true)
-
-  const decrypted = await decrypt(client, { ciphertext })
-  expect(decrypted).toBe(plaintext)
+  test('throws an error when using an invalid keyset', async () => {
+    try {
+      await newClient({
+        encryptConfig,
+        clientOpts: {
+          keyset: { Name: 'invalid' },
+        },
+      })
+    } catch (error: unknown) {
+      expect((error as Error).message.includes('Failed to load keyset')).toBe(
+        true,
+      )
+    }
+  }, 10000)
 })
